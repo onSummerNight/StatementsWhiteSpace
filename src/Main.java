06b5b980c1f8f30b9b8e91aa9297ea99fbee3ab2
BuildTools <unconfigured@null.spigotmc.org>
2023-10-07 15:03:56 +0700

    add new class (Main)

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        int myVariable = 50;
+
+        myVariable++;
+        myVariable--;
+
+        System.out.println("this is a test");
+
+        System.out.println("This is " +
+                           "another " +
+                           "still more.");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("myVariable = " + myVariable);
+
+    }
+}
